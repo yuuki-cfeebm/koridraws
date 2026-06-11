68f9f40f14e246cbccfd2c69a5009ffb23dab5e7
kaua-yanase <kaua.yanase@seriea.digital>
2026-06-11 01:54:41 -0300

    fix: config

--- js/config.js
+++ js/config.js
@@ -1,7 +1,5 @@
 const isLocalhost = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
 
-export const API_BASE_URL = 'https://koridrawsbanco.onrender.com/api'
-
-// export const API_BASE_URL = isLocalhost 
-//   ? 'https://localhost:7161/api' 
-//   : 'https://koridrawsbanco.onrender.com/api';
+export const API_BASE_URL = isLocalhost 
+  ? 'https://localhost:7161/api' 
+  : 'https://koridrawsbanco.onrender.com/api';
